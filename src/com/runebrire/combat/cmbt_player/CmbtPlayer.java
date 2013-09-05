a2dcdd69be2091bb497f44dc6b1fa6e487f4edeb
Hunter Parmley <parmleyhunt@gmail.com>
2013-09-05 17:13:23 -0500

    Started skillz

--- src/com/runebrire/combat/cmbt_player/CmbtPlayer.java
+++ src/com/runebrire/combat/cmbt_player/CmbtPlayer.java
@@ -1,11 +1,26 @@
 package com.runebrire.combat.cmbt_player;
 
+import org.bukkit.entity.Player;
+
 import com.runebrire.combat.Combat;
 
 public class CmbtPlayer {
-	
 	private Combat plugin;
+	private String playerName;
+	private int playerXP;
+	private int playerLevel;
+	//party
+	//faction
+	//String race
+	//skillsystem
+	//maxHealth
+	//maxEnergy
 	
+	public CmbtPlayer(String playerName){
+		this.playerName = playerName;
+	}
 	
-	
+	public int getNeededXP(){
+		return (100*playerLevel)-playerXP;
+	}
 }
