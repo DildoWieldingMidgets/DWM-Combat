b2bedaed7de7bf64e5d57c072db0b4f8ff59e39d
Hunter Parmley <parmleyhunt@gmail.com>
2013-09-05 19:08:59 -0500

    Removed

--- src/com/runebrire/combat/spells/EffectInstant.java
+++
@@ -1,8 +0,0 @@
-package com.runebrire.combat.spells;
-
-public class EffectInstant extends EffectDurational{
-
-	public EffectInstant(EffectCause cause, EffectType type) {
-		super(1, cause, type);
-	}
-}
